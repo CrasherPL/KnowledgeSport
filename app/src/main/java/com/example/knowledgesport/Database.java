277856c398e55f4dc347a446c07899a96a2679fa
CrasherPL <Michal_Majder@wp.pl>
2020-01-13 17:21:58 +0100

    KnowledgeSport

---
+++ app/src/main/java/com/example/knowledgesport/Database.java
@@ -0,0 +1,54 @@
+package com.example.knowledgesport;
+
+public class Database {
+
+    Integer[] athletes = {
+            R.drawable.bartosz_kurek,
+            R.drawable.bartosz_zmarzlik,
+            R.drawable.dawid_kubacki,
+            R.drawable.conor_mcgregor,
+            R.drawable.kylian_mbappe,
+            R.drawable.leo_messi,
+            R.drawable.pawel_fajdek,
+            R.drawable.robert_kubica,
+            R.drawable.robert_lewandowski,
+            R.drawable.tomasz_gollob,
+            R.drawable.james_lebron,
+            R.drawable.krzysztof_holowczyc,
+            R.drawable.mamed_khalidov,
+            R.drawable.marcin_lewandowski,
+            R.drawable.kamil_syprzak,
+            R.drawable.rafael_nadal,
+            R.drawable.novak_djokovic,
+            R.drawable.konrad_czerniak,
+            R.drawable.kacper_gomolski,
+            R.drawable.aron_chmielewski,
+
+
+    };
+
+    String[] answers = {
+            "Bartosz Kurek",
+            "Bartosz Zmarzlik",
+            "Dawid Kubacki",
+            "Conor_Mcgregor",
+            "Kylian Mbappe",
+            "Leo Messi",
+            "Paweł Fajdek",
+            "Robert Kubica",
+            "Robert Lewandowski",
+            "Tomasz Gollob",
+            "James LeBron",
+            "Krzysztof Hołowczyc",
+            "Mamed Khalidov",
+            "Marcin Lewandowski",
+            "Kamil Syprzak",
+            "Rafael Nadal",
+            "Novak Djokovic",
+            "Konrad Czerniak",
+            "Kacper Gomolski",
+            "Aron Chmielewski",
+
+    };
+
+}
---
+++ app/src/main/java/com/example/knowledgesport/ImageItem.java
@@ -0,0 +1,21 @@
+package com.example.knowledgesport;
+
+public class ImageItem {
+
+    String name;
+    int image;
+
+    public ImageItem(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
+
